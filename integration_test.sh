63ed3d2e33ebcf2d22d82dd9bc7591bd39976ad1
Paul Williams <paulswilliams@me.com>
2013-11-28 20:59:57 +0000

    refactored integration script

--- integration_test.sh
+++ integration_test.sh
@@ -1,23 +1,48 @@
 #!/bin/bash 
-echo 'cleaning...'
+
+function clean {
+  echo 'cleaning...'
   rm textin
   rm textout
-  rm test_output.bmp
-echo 'compile'
+  rollback
+}
+
+function rollback {
+  echo 'rolling back...'
+
+  if [ -f 'test_output.bmp' ]; then
+    rm test_output.bmp
+  fi
+}
+
+function compile {
+  echo 'compiling...'
   cc -o textin textin.c
   cc -o textout textout.c
+}
+
+clean
+compile
+
 echo 'testing textin creates a file'
-  ./textin test_input.bmp test_output.bmp 'fred'
-  if [ -f 'test_output.bmp' ]; then
-    echo 'PASS: test_output.bmp was created'
-  else
-    echo 'FAIL: test_output.bmp was not created'
-  fi
+./textin test_input.bmp test_output.bmp 'fred'
+if [ -f 'test_output.bmp' ]; then
+  echo 'PASS: test_output.bmp was created'
+else
+  echo 'FAIL: test_output.bmp was not created'
+fi
+
+rollback
+
 echo 'testing textout outputs message from a file'
-  retreived_text=$(./textout fixture_image.bmp)
-  if [ "$retreived_text" == "Hello World" ]; then
-    result="PASS"
-  else
-    result="FAIL"
-  fi
-  echo $result $retreived_text " was output"
+retreived_text=$(./textout fixture_image.bmp)
+if [ "$retreived_text" == "Hello World" ]; then
+  result="PASS"
+else
+  result="FAIL"
+fi
+echo $result $retreived_text " was output"
+
+rollback
+
+echo "Completed"
